b27f8308e2c9a9facd38e168bc7ff5385fbe27ad
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-01-29 23:36:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>Helloworld</header>
+
+  <main>Helloworld2</main>
+
+  <footer>
+    <p>Edit <code>src/App.vue</code> and save to reload.</p>
+  </footer>
+</template>
+
+<style scoped></style>
