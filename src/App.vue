78a97b7e8a8c6bf569ffab555d7d63a459599ef2
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-01-31 13:49:51 +0700

    feat: add site speed insight from Vercel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import AppFooter from './components/AppFooter.vue'
 import AppVisualizer from './components/AppVisualizer.vue'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 </script>
 
 <template>
   <div class="wrapper">
+    <SpeedInsights />
     <AppVisualizer />
     <AppFooter />
   </div>
